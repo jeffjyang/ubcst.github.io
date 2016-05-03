6b0ee3503f06ae370621d512c03316cf5301f7a8
Derek Chen <me@derekychen.com>
2016-05-02 17:28:24 -0700

    Fixed carousel buttons on mobile displays, changed link boxes to display two per row on mobile, cropped team picture.

--- js/slick.js
+++ js/slick.js
@@ -10,22 +10,26 @@ $('.responsive').slick({
     {
       breakpoint: 1024,
       settings: {
+        nextArrow: '<button type="button" class="slick-next"><span class="glyphicon glyphicon-chevron-right"></span>&nbsp;</button>',
+        prevArrow: '<button type="button" class="slick-prev"><span class="glyphicon glyphicon-chevron-left"></span>&nbsp;</button>',
         slidesToShow: 3,
-        slidesToScroll: 3,
-        infinite: true,
-        dots: true
+        slidesToScroll: 1
       }
     },
     {
       breakpoint: 600,
       settings: {
+        nextArrow: '<button type="button" class="slick-next"><span class="glyphicon glyphicon-chevron-right"></span>&nbsp;</button>',
+        prevArrow: '<button type="button" class="slick-prev"><span class="glyphicon glyphicon-chevron-left"></span>&nbsp;</button>',
         slidesToShow: 2,
-        slidesToScroll: 2
+        slidesToScroll: 1
       }
     },
     {
       breakpoint: 480,
       settings: {
+        nextArrow: '<button type="button" class="slick-next"><span class="glyphicon glyphicon-chevron-right"></span>&nbsp;</button>',
+        prevArrow: '<button type="button" class="slick-prev"><span class="glyphicon glyphicon-chevron-left"></span>&nbsp;</button>',
         slidesToShow: 1,
         slidesToScroll: 1
       }
